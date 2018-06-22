33af8be0c41cdf7f4a77c3d309a830b630fdbf73
botvinick <benbotvinick@gmail.com>
2018-06-22 15:03:26 -0400

    Add forcedomain protocol
    Signed-off-by: botvinick <benbotvinick@gmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var forcedomain = require('forcedomain');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(forcedomain({
+	hostname: 'unlit.io',
+	protocol: 'https'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
