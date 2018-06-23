4f8776a2e80c85a4b8a94bd3f219355a0fbc6db2
botvinick <benbotvinick@gmail.com>
2018-06-22 21:03:32 -0400

    Jquery zoom and stuff
    Signed-off-by: botvinick <benbotvinick@gmail.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,18 @@ require('particles.js');
 require('fullpage.js');
 const $ = require('jquery');
 
+$(document).keydown(function (event) {
+	if (event.ctrlKey === true && (event.which === '61' || event.which === '107' || event.which === '173' || event.which === '109' || event.which === '187' || event.which == '189')) {
+		event.preventDefault();
+	}
+});
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+	if (event.ctrlKey === true) {
+		event.preventDefault();
+	}
+});
+
 $("body").on('mouseover', 'a', function (e) {
 	var link = $(this);
 	var href = link.attr('href') || link.data("href");
