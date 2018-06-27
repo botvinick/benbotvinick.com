c2c136dce92f5bdeddf222a9c8d39c32a01375e5
Ben Botvinick <benbotvinick@gmail.com>
2018-06-26 22:39:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(forcedomain({
-	hostname: 'btvnk.io',
+	hostname: 'benbotvinick.com',
 	protocol: 'https'
 }));
 
