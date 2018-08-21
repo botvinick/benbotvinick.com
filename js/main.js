bfbe4e28d54636e9507a6ee8253fdc18609373a0
Ben Botvinick <benbotvinick@gmail.com>
2018-08-21 10:38:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 require('particles.js');
 
+$(function () {
+	console.log('I love this font so much')
+	console.log('It\'s really quite beautiful')
+	console.log('If you wanna talk to me email me at one of the following addresses:\n')
+	console.log('ben@benbotvinick.com')
+	console.log('ben@unlit.io')
+	console.log('ben@bough.io')
+})
+
 $(document).keydown(function (event) {
 	if (event.ctrlKey === true && (event.which === '61' || event.which === '107' || event.which === '173' || event.which === '109' || event.which === '187' || event.which == '189')) {
 		event.preventDefault();
